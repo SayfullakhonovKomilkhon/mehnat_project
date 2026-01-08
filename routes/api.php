f4b709ed833e79def88b16725e8227ec1c98ea67
Mac <sayfullakhonovkomilkhon@gmail.com>
2026-01-08 22:05:39 +0500

    Add temporary endpoint to run article comments seeder

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,28 @@ Route::prefix('v1')->group(function () {
     
     /*
     |--------------------------------------------------------------------------
+    | Temporary: Run Article Comments Seeder
+    | DELETE THIS ROUTE AFTER USE!
+    |--------------------------------------------------------------------------
+    */
+    Route::get('/run-article-comments-seeder', function () {
+        try {
+            \Artisan::call('db:seed', ['--class' => 'AuthorExpertSeeder', '--force' => true]);
+            return response()->json([
+                'success' => true,
+                'message' => 'Article comments seeder completed',
+                'output' => \Artisan::output()
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    });
+    
+    /*
+    |--------------------------------------------------------------------------
     | Public Routes - No Authentication Required
     |--------------------------------------------------------------------------
     */
